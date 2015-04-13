0f7a980cf9dfeb32fd0cc46c07d267f325861cb9
Adam Tannir <atannir@vonaco>
2015-04-13 15:48:12 -0400

    Trying again due to not including one file in commit.

--- Tile.java
+++ Tile.java
@@ -1,9 +1,47 @@
 package atannir.puzzle.nosuchtiles;
 
-class Tile {
+public class Tile {
     
-    //allergies are too bad to think about how to fill this in right now
+    private int num = 0; //tile number for tracking
+    private String[] Trbl = new String[4]; //Top Right Bottom Left
+    private int rot = 0; // mod 4, rotation counter, CW assumed here
+
+    //enums here later, maybe, but now, strings.
     
+
+
+    public Tile(int n, String T, String R, String B, String L){
+	if(n > 0 && n <= 9) { num = n; }
+	else n = -1;
+	if (2 == T.length() && 2 == R.length() && 2 == B.length() && 2 == L.length()){
+	    //Trbl = { T, R, B, L}; //doesn't work, even with [].
+	    Trbl[0] = T;
+	    Trbl[1] = R;
+	    Trbl[2] = B;
+	    Trbl[3] = L;
+	}
+	//else? We'll try not to misbehave.	
+
+    }
     
-    
+    //rotate
+    public int rotateCW(int n) {
+	rot = rot + (n % 4); // equivalent to (rot + n) % 4, right?
+	return rot;
+    }
+    public int rotate() { return rotateCW(1);}
+
+    public String toString() {
+	//TODO add offset into this so it is useful
+	return new String("Tile " + num + " " + 
+			  Trbl[0] + " " +
+			  Trbl[1] + " " + 
+			  Trbl[2] + " " +
+			  Trbl[3]);
+    }
+
+
+    //get(trbl) etc
+    //toString
+
 }
---
+++ TileDriver.java
@@ -0,0 +1,24 @@
+package atannir.puzzle.nosuchtiles;
+
+import atannir.puzzle.nosuchtiles.*;
+//import Tile;
+
+
+
+//solver (and tester) for the Tile class and the puzzle at large
+
+
+public class TileDriver {
+    
+    
+    public static void main(String args[]) {
+	
+	//Tile t = new Tile(1, "FT", "RB", "VT", "MB");
+	
+	//System.out.println(t.toString());
+	System.out.println("TEST");
+	
+	
+    }
+    
+}
