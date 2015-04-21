e2ae264a9c5cbe45bc0288a198aba78bf6fa958b
Adam Tannir <atannir@gmail.com>
2015-04-21 18:40:24 -0400

    Hooray for the debugger!

--- Tile.java
+++ Tile.java
@@ -1,44 +1,45 @@
 //package atannir.puzzle.nosuchtiles;
 
 public class Tile {
-    
-    private int num = 0; //tile number for tracking
-    private String[] Trbl = new String[4]; //Top Right Bottom Left
-    private int rot = 0; // mod 4, rotation counter, CW assumed here
 
-    //enums here later, maybe, but now, strings.
-    
+	private int num = 0; //tile number for tracking
+	private String[] Trbl = new String[4]; //Top Right Bottom Left
+	private int rot = 0; // mod 4, rotation counter, CW assumed here
+
+	//enums here later, maybe, but now, strings.
 
 
-    public Tile(int n, String T, String R, String B, String L){
-	if(n > 0 && n <= 9) { num = n; }
-	else n = -1;
-	if (2 == T.length() && 2 == R.length() && 2 == B.length() && 2 == L.length()){
-	    //Trbl = { T, R, B, L}; //doesn't work, even with [].
-	    Trbl[0] = T;
-	    Trbl[1] = R;
-	    Trbl[2] = B;
-	    Trbl[3] = L;
+
+	public Tile(int n, String T, String R, String B, String L){
+		if(n > 0 && n <= 9) { num = n; }
+		else n = -1;
+		if (2 == T.length() && 2 == R.length() && 2 == B.length() && 2 == L.length()){
+			//Trbl = { T, R, B, L}; //doesn't work, even with [].
+			Trbl[0] = T;
+			Trbl[1] = R;
+			Trbl[2] = B;
+			Trbl[3] = L;
+		}
+		//else? We'll try not to misbehave.
+
 	}
-	//else? We'll try not to misbehave.	
 
-    }
-    
-    //rotate
-    public int rotateCW(int n) {
-	rot = (4 - ((rot + n) % 4));
-	return rot;
-    }
-    public int rotate() { return rotateCW(1);}
+	//rotate
+	public int rotateCW(int n) {
+		if(rot == 0) {rot = 4;} //stupid but it works.
+		rot = (rot - n);
+		return rot;
+	}
+	public int rotate() { return rotateCW(1);}
 
-    public String toString() {
-	//index plus offset mod 4
-	return new String("Tile " + num + " " + 
-			  Trbl[(((0 + rot) % 4))] + " " +
-			  Trbl[(((1 + rot) % 4))] + " " + 
-			  Trbl[(((2 + rot) % 4))] + " " +
-			  Trbl[(((3 + rot) % 4))]);
-    }
+	public String toString() {
+		//index plus offset mod 4
+		return new String("Tile " + num + " " +
+				Trbl[(((0 + rot) % 4))] + " " +
+				Trbl[(((1 + rot) % 4))] + " " +
+				Trbl[(((2 + rot) % 4))] + " " +
+				Trbl[(((3 + rot) % 4))]);
+	}
 
 	public String getN(String pos){
 		int idx = 0;
