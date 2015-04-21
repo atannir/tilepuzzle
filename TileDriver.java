5048cee6939425720dde3564dc012da1e48888c4
Adam Tannir <atannir@gmail.com>
2015-04-21 18:39:47 -0400

    Hooray for the debugger!

--- TileDriver.java
+++ TileDriver.java
@@ -9,24 +9,23 @@
 
 
 public class TileDriver {
-    
-    
-    public static void main(String[] args) {
-	
-	Tile t = new Tile(1, "FT", "RB", "VT", "MB");
-	System.out.println(t.toString());
-	t.rotate();
-	System.out.println(t.toString());
-	t.rotate();
-	//currently something goes wrong right about here.
-	System.out.println(t.toString());
-	t.rotate();
-	System.out.println(t.toString());
-	System.out.println(t.getT());
-	
-	//System.out.println(t.toString());
-	System.out.println("TEST");
-	
-	
-    }
+
+
+	public static void main(String[] args) {
+
+		Tile t = new Tile(1, "FT", "RB", "VT", "MB");
+		System.out.println(t.toString());
+		System.out.println(t.rotate());
+		System.out.println(t.toString());
+		System.out.println(t.rotate());
+		System.out.println(t.toString());
+		System.out.println(t.rotate());
+		System.out.println(t.toString());
+		System.out.println(t.getT()); // no longer correct
+
+		//System.out.println(t.toString());
+		System.out.println("TEST");
+
+
+	}
 }
